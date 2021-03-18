00720f80e6b55398c5223cad679274052e5b1242
Marcel Kollovieh <marcel.kollovieh@tum.de>
2021-03-18 23:04:26 +0100

    Bugfix for parameters

--- tumor_surrogate_pytorch/neural_inference/posterior.py
+++ tumor_surrogate_pytorch/neural_inference/posterior.py
@@ -6,15 +6,17 @@ from sbi.inference import simulate_for_sbi, prepare_for_sbi, APT
 
 def simulate_parameters_from_uniform(parameters):
     parameters[0] = parameters[0] * 0.0007 + 0.0001
-    parameters[2] = parameters[1] * 0.0299 + 0.0001
-    parameters[3] = int(parameters[2] * 20 + 1)
+    parameters[1] = parameters[1] * 0.0299 + 0.0001
+    parameters[2] = int(parameters[2] * 20 + 1)
+    parameters[6] = parameters[6] * 0.2 + 0.6
+    parameters[7] = parameters[7] * 0.55 + 0.05
     return parameters
 
 
 class NPE:
     def __init__(self, simulator):
-        self.prior = utils.BoxUniform(low=torch.zeros(6),
-                                 high=torch.ones(6))
+        self.prior = utils.BoxUniform(low=torch.zeros(8),
+                                 high=torch.ones(8))
         self.simulator = simulator
         self.posteriors = []
 
