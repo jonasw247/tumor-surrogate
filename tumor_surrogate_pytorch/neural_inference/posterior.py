dc2021ba66ba3d8f2bba1a38d5c880125bc2669e
Chantal Pellegrini <chantal.pellegrini@gmail.com>
2021-03-18 17:53:34 +0100

    - uncrop net output - main method

--- tumor_surrogate_pytorch/neural_inference/posterior.py
+++ tumor_surrogate_pytorch/neural_inference/posterior.py
@@ -3,12 +3,8 @@ import torch
 from sbi import utils
 from sbi.inference import simulate_for_sbi, prepare_for_sbi, APT
 
-
-def simulate_parameters_from_uniform(parameters):
-    parameters[0] = parameters[0] * 0.0007 + 0.0001
-    parameters[2] = parameters[1] * 0.0299 + 0.0001
-    parameters[3] = int(parameters[2] * 20 + 1)
-    return parameters
+from tumor_surrogate_pytorch.neural_inference.simulator import Simulator
+import numpy as np
 
 
 class NPE:
@@ -22,12 +18,18 @@ class NPE:
         inference = APT(prior=self.prior)
         simulator, prior = prepare_for_sbi(self.simulator, self.prior)
         proposal = prior
+        posterior = None
         for _ in range(num_rounds):
             theta, x = simulate_for_sbi(simulator, proposal, num_simulations=num_simulations)
             density_estimator = inference.append_simulations(theta, x, proposal=proposal).train()
             posterior = inference.build_posterior(density_estimator)
             self.posteriors.append(posterior)
             proposal = posterior.set_default_x(x_ob)
+        return posterior
 
 if __name__ == '__main__':
-    print("hier")
+    simulator = Simulator()
+    npe = NPE(simulator = simulator)
+    x_ob = np.load('tumor_surrogate_pytorch/neural_inference/x_obs_test.npz')
+    x_ob = x_ob['x_025'] + x_ob['x_07']
+    posterior = npe.forward(x_ob=x_ob, num_rounds=10, num_simulations=500)
